9382088cf9df3ec2ffd0a6386b0719f568bd4550
Chima_9226 <iheanachochima7@gmail.com>
2026-08-05 17:47:04 +0100

    new changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
   base: '/FOHG/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(process.cwd(), 'src'),
     },
   },
   optimizeDeps: {
